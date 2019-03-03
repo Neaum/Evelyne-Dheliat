22b3e4402275a5d6d44eff0c2da20c0b763ac584
SamLef <43677207+SamLef@users.noreply.github.com>
2019-03-03 21:39:29 +0100

    Update LoLin

--- PROJECT/LoLin_Sigfox.py
+++ PROJECT/LoLin_Sigfox.py
@@ -1,28 +1,25 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+import time
 import paho.mqtt.publish as mqtt
-import json
-import datetime
+
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=['POST'])
 
 def data():
-    body = request.json
-    if body == None:
-        return "not a json"
-    if 'data' in body.keys():
-        print("MessageRecu")
-        #receive_data temperature and humidity
-        Temp=str(getTemp())
-        Hum = str(getHum())
-        #creation message mqtt to send
-        MESSAGE=Temp+" "+Hum
-        print(MESSAGE)
-        #send message
-        mqtt.single("Value",MESSAGE)
-    return jsonify(body)
+	body = request.json
+	if body == None:
+		return "not a json"
+	if 'data' in body.keys():
+		mqtt.single(body["data"])
 
-app.run(debug=False)
+
+	return jsonify(body)
+
+app.run(debug=True)
+
+data()
