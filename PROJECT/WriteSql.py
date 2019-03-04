e1c48d25b669478126829bc8e318de5357831d46
Neaum <loic.tang@isen.yncrea.fr>
2019-03-04 02:56:58 +0100

    modifications meteo

--- PROJECT/WriteSql.py
+++ PROJECT/WriteSql.py
@@ -1,6 +1,7 @@
 import datetime
 import os.path
 import paho.mqtt.client as mqtt
+from statistics import mean
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -10,13 +11,59 @@ def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     date = datetime.datetime.now().isoformat()
     realdate = date.split("T")
+    realdate="'"+realdate[0]+"'"
     temp=(msg.payload.split()[0])
     hum=(msg.payload.split()[1])
-    f = open("meteo.txt","a")
-    f.write("'"+realdate[0]+"'"+" "+str(temp.decode("utf-8"))+" "+str(hum.decode("utf-8"))+"\n")
+    fr=open("meteo.txt","r")
+    contenu=""
+    j=0
+    for lignes in fr:
+        if(j==0):
+            old=lignes
+        elif(j!=6):
+            contenu+=lignes
+        elif(j==6):
+            lastdate=lignes.split(" ")
+            lastligne=lignes
+        j=j+1
+    fr.close()
+    f = open("meteo.txt","w")
+    if(lastdate[0] != realdate):   #Data on other  day
+        print("newd")
+        f.write(contenu)
+        f.write(lastligne)
+        f.write(realdate+" "+str(temp.decode("utf-8"))+" "+str(hum.decode("utf-8"))+"\n")    #Add new data
+        list_temp[:]=[]
+        list_hum[:]=[]
+        list_temp.append(float(temp.decode("utf-8")))
+        list_hum.append(float(hum.decode("utf-8")))
+    elif(lastdate[1]==str(0)):  #Still the init data
+        print("newi")
+        f.write(contenu)
+        f.write(lastligne)
+        f.write(realdate+" "+str(temp.decode("utf-8"))+" "+str(hum.decode("utf-8"))+"\n")    #Add new data
+        list_temp[:]=[]
+        list_hum[:]=[]
+        list_temp.append(float(temp.decode("utf-8")))
+        list_hum.append(float(hum.decode("utf-8")))
+    else:   #Data on the same day
+        print("old")
+        list_temp.append(float(temp.decode("utf-8")))
+        list_hum.append(float(hum.decode("utf-8")))
+        f.write(old)
+        f.write(contenu)
+        f.write(realdate+" "+str(mean(list_temp))+" "+str(mean(list_hum))+"\n")
     f.close()
 
+#init .txt database
+if(os.path.isfile("meteo.txt")==False):
+    f = open('meteo.txt', 'a')
+    for i in range(0,7):
+        f.write("'"+(datetime.datetime.now().isoformat()).split("T")[0]+"'"+" "+str(0)+" "+str(0)+"\n")
+    f.close()
 
+list_temp=[]
+list_hum=[]
 client=mqtt.Client()
 client.on_connect=on_connect
 client.on_message=on_message
